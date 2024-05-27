ff9c494e5eb9b25105d1d64b4e0a08028481034c
Hoai Duc <mac@Macs-MacBook-Pro.local>
2024-05-27 08:51:38 +0700

    Add logic game

--- src/components/tictactoe/Board.js
+++ src/components/tictactoe/Board.js
@@ -1,17 +1,43 @@
-import React from 'react';
-import Cell from './Cell';
-import "./GameStyle.css"
+import { React, useState } from "react";
+import Cell from "./Cell";
+import Menu from "./Menu";
+import "./GameStyle.css";
+import { calcWinner } from "../../services/CalcGame";
 
 const Board = () => {
-	return (
-		<div className='game-board'>
-			{
-				Array(9).fill().map((item, index) => (
-					<Cell key={index}></Cell>
-				))
-			}
-		</div>
-	);
+  const [cells, setCells] = useState(Array(9).fill(null));
+  const [currentPlayer, setCurrentPlayer] = useState("X");
+
+  const handleClick = (index) => {
+    if (cells[index] || calcWinner(cells)) return;
+
+    const newCells = cells.slice();
+    newCells[index] = currentPlayer;
+    setCells(newCells);
+    setCurrentPlayer(currentPlayer === "X" ? "O" : "X");
+  };
+
+  const restartGame = () => {
+    setCells(Array(9).fill(null));
+    setCurrentPlayer("X");
+  }
+
+  const renderCell = (index) => {
+    return <Cell value={cells[index]} onClick={() => handleClick(index)} />;
+  };
+
+  return (
+    <div className="game-container">
+      <div className="game-board">
+        {cells.map((_cell, index) => (
+          <div key={index} className="board-row">
+            {renderCell(index)}
+          </div>
+        ))}
+      </div>
+        <Menu restartGame={restartGame}></Menu>
+    </div>
+  );
 };
 
 export default Board;
--- src/components/tictactoe/Cell.js
+++ src/components/tictactoe/Cell.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import "./GameStyle.css"
 
 const Cell = (props) => {
   return (
     <div className='game-cell' onClick={props.onClick}>
-      
+      {props.value}
     </div>
   );
 };
